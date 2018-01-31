49fde6fead276025b1b24d586b233655bf77287a
luigisamurai <labs.sistemas@gmail.com>
2018-01-31 11:32:34 -0500

    my firts pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,57 @@
 #!/usr/bin/env groovy
+import hudson.model.ParameterValue;
+import hudson.model.ParametersAction;
+
+@Field final String TEST_ENV_PARAMETER = 'PIPELINE_ENV'
 
 def execute() {
-  echo("${env.WORKSPACE}/stages/test.groovy")
+    def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
+    // def allParameters = []
+
+    // def pipelineParameter =  string(
+    //     name: TEST_ENV_PARAMETER,
+    //     description: 'Pipeline Environment Config key',
+    //     defaultValue: env.DEFAULT_PIPELINE_ENV
+    // )
+
+    // allParameters.add(pipelineParameter)
+
+    for(ParameterValue parameter in jenkinsfileParameters) {
+        println parameter.name
+        println parameter.getDefinition().getType()
+        // def item
+
+        // if (parameter.choices) {
+        //     item = choice(
+        //         name: parameter.name,
+        //         description: description: parameter.description,
+        //         choices: parameter.choices,
+        //         defaultValue: parameter.defaultValue
+        //     )
+        // } else if () {
+
+        // } else {
+        //     item = string(
+        //         name: parameter.name,
+        //         description: parameter.description,
+        //         defaultValue: parameter.defaultValue
+        //     )
+        // }
+        // allParameters.add(item)
+    }
+
+    // properties([
+    //     buildDiscarder(
+    //         logRotator(artifactDaysToKeepStr: '', 
+    //                 artifactNumToKeepStr: '', 
+    //                 daysToKeepStr: env.DISCARD_DAYS, 
+    //                 numToKeepStr: env.DISCARD_NUMBER_BUILDS)),
+    //     parameters(allParameters)
+    // ])
+
   stage('test') {
     echo('Hello, it is my firts multi branch pipeline.')
+    stageDefinition.executeStage()
   }
 }
 
