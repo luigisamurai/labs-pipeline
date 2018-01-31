27a80abd27635ca9c524ad818b3a907a469b0a85
luigisamurai <labs.sistemas@gmail.com>
2018-01-31 11:21:12 -0500

    my firts pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,9 @@
 #!/usr/bin/env groovy
 
 def execute() {
- def stageDefinition = load("stages/test.groovy")
   echo("${env.WORKSPACE}/stages/test.groovy")
   stage('test') {
-    stageDefinition.executeStage()
+    echo('Hello, it is my firts multi branch pipeline.')
   }
 }
 
