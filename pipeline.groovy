e3e6bc3653011db1761954281df4f20addaee751
luigisamurai <labs.sistemas@gmail.com>
2018-01-31 11:19:41 -0500

    my firts pipeline

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,7 +1,7 @@
 #!/usr/bin/env groovy
 
 def execute() {
- def stageDefinition = load("${env.WORKSPACE}/stages/test.groovy")
+ def stageDefinition = load("stages/test.groovy")
   echo("${env.WORKSPACE}/stages/test.groovy")
   stage('test') {
     stageDefinition.executeStage()
