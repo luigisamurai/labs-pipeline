6f937760a25d66458f10a8d804fc1d80065802e6
luigisamurai <labs.sistemas@gmail.com>
2018-01-31 13:47:50 -0500

    add jenkinsfile

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
 import hudson.model.ParameterValue;
 import hudson.model.ParametersAction;
-import hudson.model.ParameterDefinition
+// import hudson.model.ParameterDefinition
 
 // @Field final String TEST_ENV_PARAMETER = 'PIPELINE_ENV'
 
 def execute() {
-    def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParameterDefinition.class)
-    def jenkinsbuilds = currentBuild.rawBuild.getAction(BuildDiscarder.class)
+    // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParameterDefinition.class)
+    // def jenkinsbuilds = currentBuild.rawBuild.getAction(BuildDiscarder.class)
 
     // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
     // def allParameters = []
@@ -53,10 +53,15 @@ def execute() {
     //     parameters(allParameters)
     // ])
 
-    properties([
-        buildDiscarder(jenkinsbuilds),
-        parameters(jenkinsfileParameters)
-    ])
+    // properties([
+    //     buildDiscarder(jenkinsbuilds),
+    //     parameters(jenkinsfileParameters)
+    // ])
+
+    
+    parameters {
+        booleanParam(defaultValue: true, description: '', name: 'userFlag')
+    }
 
   stage('test') {
     echo('Hello, it is my firts multi branch pipeline.')
