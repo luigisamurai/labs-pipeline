afb32ad07058c9eaccfe0887c87fbdd6164ed19b
luigisamurai <labs.sistemas@gmail.com>
2018-01-31 14:04:34 -0500

    add jenkinsfile

--- pipeline.groovy
+++ pipeline.groovy
@@ -6,7 +6,7 @@ import hudson.model.ParametersAction;
 // @Field final String TEST_ENV_PARAMETER = 'PIPELINE_ENV'
 
 def execute() {
-    // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParameterDefinition.class)
+    // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
     // def jenkinsbuilds = currentBuild.rawBuild.getAction(BuildDiscarder.class)
 
     // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
@@ -58,8 +58,20 @@ def execute() {
     //     parameters(jenkinsfileParameters)
     // ])
 
-  def newProperties = properties().toArray()
-  properties(newProperties)
+  def changeLogSets = currentBuild.changeSets
+  for (int i = 0; i < changeLogSets.size(); i++) {
+      def entries = changeLogSets[i].items
+      for (int j = 0; j < entries.length; j++) {
+          def entry = entries[j]
+          echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+          def files = new ArrayList(entry.affectedFiles)
+          for (int k = 0; k < files.size(); k++) {
+              def file = files[k]
+              echo "  ${file.editType.name} ${file.path}"
+          }
+      }
+  }
+
   stage('test') {
     echo('Hello, it is my firts multi branch pipeline.')
   }
