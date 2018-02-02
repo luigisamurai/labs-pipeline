cd740273fa1b53e3d0947821a224aba1f7d8b747
luigisamurai <labs.sistemas@gmail.com>
2018-02-02 13:59:53 -0500

    add parameters

--- pipeline.groovy
+++ pipeline.groovy
@@ -3,12 +3,17 @@
 def execute(pipelineProperties) {
 
   def pipelineParameter =  string(
-        name: 'MY_VARIABLE',
-        description: 'Pipeline Environment Config key',
-        defaultValue: ''
-    )
-  pipelineProperties[0].add(pipelineParameter)
-  properties(pipelineProperties)
+      name: 'MY_VARIABLE',
+      description: 'Pipeline Environment Config key',
+      defaultValue: ''
+  )
+
+  for (index = 0; index < pipelineProperties.size(); index++) {
+    println pipelineProperties[index]
+  }
+  
+  // pipelineProperties[0]
+  // properties(pipelineProperties)
 
   stage('test') {
      echo("Hello, it is my firts multi branch pipeline custom. ${env.MY_CUSTOM_PROP}")
