f08d02e64db9e3528d78cde3a2f8c29fc972fe04
luigisamurai <labs.sistemas@gmail.com>
2018-02-02 15:15:22 -0500

    add parameters

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,39 +1,19 @@
 #!/usr/bin/env groovy
 
 def execute(pipelineProperties) {
-
-  def pipelineParameter =  [
-    string(
-        name: 'MY_VARIABLE',
-        description: 'Pipeline Environment Config key',
-        defaultValue: ''
-    )
-  ]
-
-  def parameters = [
-    string(
-        name: 'MY_VARIABLE',
-        description: 'Pipeline Environment Config key',
-        defaultValue: ''
-    )
-  ]
-
-  // for (index = 0; index < pipelineProperties.size(); index++) {
-  //   println pipelineProperties[index].toString().startsWith("@parameters")
-  // }
-  def prop = []
-
-  for (ParametersDefinitionProperty property in pipelineProperties) {
-    if ( property.toString().startsWith("@parameters") ) {
-      println property instanceof Collection
-      println property instanceof Set
-    } else {
-      prop.add(property)
-    }
-  }
+        println pipelineProperties.getClass()
+        
+        def abcd = parameters ([
+        string(
+            name: 'PIPELINE_ENV',
+            description: 'Specify the environment to be run, by default it executes stg. for example: qa, stg, prod, onprem, eu',
+            defaultValue: 'stg.....'
+        )
+    ])
+    
+    pipelineProperties.add(abcd)
   
-  // pipelineProperties[0]
-  properties(prop)
+  properties(pipelineProperties)
 
   stage('test') {
      echo("Hello, it is my firts multi branch pipeline custom. ${env.MY_CUSTOM_PROP}")
