401c7e59fa124bbc687e229d941748db7ae95eae
luigisamurai <labs.sistemas@gmail.com>
2018-02-01 21:18:48 -0500

    add parameters

--- pipeline.groovy
+++ pipeline.groovy
@@ -21,6 +21,17 @@ def execute() {
   // currentBuild.rawBuild.addAction(pa)
 
 
+  // def pa = new ParametersAction([
+  //   new StringParameterValue("FOO", "BAR")
+  // ])
+  // currentBuild.add(pa)
+
+  def parameters = currentBuild.getBadgeActions()
+
+  properties([
+    parameters (parameters)
+  ])
+
   stage('test') {
     echo("Hello, it is my firts multi branch pipeline. ${env.FOO}")
   }
