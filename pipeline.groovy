54bd4d630796f58508262cd2e467c9b05248be10
luigisamurai <labs.sistemas@gmail.com>
2018-02-05 08:11:33 -0500

    add parameters

--- pipeline.groovy
+++ pipeline.groovy
@@ -9,39 +9,46 @@ def execute(pipelineProperties) {
   //           defaultValue: 'stgXDR.....'
   //       )
   //   ])
+
+  def item = string(
+      name: 'PIPELINE_ENV_DEFAULT',
+      description: 'Specify the environment to be run, by default it executes stg. for example: qa, stg, prod, onprem, eu',
+      defaultValue: 'stgXDR.....'
+  )
     
   // //   pipelineProperties.add(abcd)
   
   // // properties(pipelineProperties)
 
-  // def prop = []
+  def prop = []
   // def allParameters = []
 
 
   // allParameters.add(pipelineParameter)
 
-  // for (ParametersDefinitionProperty property in pipelineProperties) {
-  //   if ( property.toString().startsWith("@parameters") ) {
-  //       // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
-  //       // for(ParameterValue parameter in jenkinsfileParameters) {
-  //       //   def item = string(
-  //       //       name: parameter.name,
-  //       //       description: parameter.description
-  //       //   )
-  //       //   allParameters.add(item)
-  //       // }
+  for (ParametersDefinitionProperty property in pipelineProperties) {
+    if ( property.toString().startsWith("@parameters") ) {
+        // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
+        // for(ParameterValue parameter in jenkinsfileParameters) {
+        //   def item = string(
+        //       name: parameter.name,
+        //       description: parameter.description
+        //   )
+        //   allParameters.add(item)
+        // }
 
-  //       // properties([
-  //       //     parameters(allParameters)
-  //       // ])
-  //       // prop.add(property.getArguments())
-  //       // println property.getArguments()
-        
-      
-  //   } else {
-  //     prop.add(property)
-  //   }
-  // }
+        // properties([
+        //     parameters(allParameters)
+        // ])
+        // prop.add(property.getArguments())
+        // println property.getArguments()
+        property.add(item)
+        prop.add(property)
+
+    } else {
+      prop.add(property)
+    }
+  }
 
   // def jenkinsfileParameters = currentBuild.rawBuild.getAction(ParametersAction.class)
   // def allParameters = []
@@ -60,19 +67,13 @@ def execute(pipelineProperties) {
   //   allParameters.add(item)
   // }
 
-  def allParameters = []
 
-  string(
-      name: 'PIPELINE_ENV_DEFAULT',
-      description: 'Specify the environment to be run, by default it executes stg. for example: qa, stg, prod, onprem, eu',
-      defaultValue: 'stgXDR.....'
-  )
 
-  for (ParametersDefinitionProperty property in pipelineProperties) {
-    allParameters.add(property)
-  }
+  // for (ParametersDefinitionProperty property in pipelineProperties) {
+  //   allParameters.add(property)
+  // }
 
-  properties(allParameters)
+  // properties(pipelineProperties)
 
   stage('test') {
      echo("Hello, it is my firts multi branch pipeline custom. ${env.PIPELINE_ENV_DEFAULT}")
